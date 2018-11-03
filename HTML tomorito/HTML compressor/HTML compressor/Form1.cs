dbd60c98d28a0ef091df0690c1272cde4915b3c9
Józsa Dávid <44705915+AnonymDavid@users.noreply.github.com>
2018-11-03 01:50:43 +0100

    Added comments
    Added comments for the main program.

--- HTML tomorito/HTML compressor/HTML compressor/Form1.cs
+++ HTML tomorito/HTML compressor/HTML compressor/Form1.cs
@@ -26,6 +26,7 @@ namespace HTML_compressor
         {
             compressData = new List<string[]>();
 
+            // adding the keys (kulcsok hozzáadása)
             #region tagek
             addCompressText("<A", @"\0");
             addCompressText("<B", @"\1");
@@ -411,6 +412,8 @@ namespace HTML_compressor
 
         private void btnForras_Click(object sender, EventArgs e)
         {
+            // original file browsing (eredeti file kiválasztása)
+
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "HTML|*.html";
             ofd.Multiselect = false;
@@ -422,6 +425,8 @@ namespace HTML_compressor
 
         private void btnCel_Click(object sender, EventArgs e)
         {
+            // place selection for the compressed file (helyválasztás a tömörített filenak)
+
             SaveFileDialog sfd = new SaveFileDialog();
             sfd.Filter = "HTML|*.html";
             if (sfd.ShowDialog() == DialogResult.OK)
@@ -432,16 +437,20 @@ namespace HTML_compressor
 
         private void btnCompress_Click(object sender, EventArgs e)
         {
+            // compressing (tömörítés)
             compress();
         }
 
         private void btnDeCompress_Click(object sender, EventArgs e)
         {
+            // decompressing (visszafejtés)
             deCompress();
         }
 
         private void addCompressText(string _base, string _compressed)
         {
+            // method for uploading the array (metódus a tömb feltöltéséhez)
+
             string[] temp = new string[2];
             temp[0] = _base;
             temp[1] = _compressed;
@@ -450,32 +459,44 @@ namespace HTML_compressor
 
         private void compress()
         {
+            // compressing method (tömörítő metódus)
             try
             {
+                // set source and destination (forrás és cél beolvasása)
                 source = txtForras.Text;
                 destination = txtCel.Text;
+
+                // check source and destination (forrás és cél ellenőrzése)
                 if (source != null && destination != null && File.Exists(source))
                 {
+                    // create destination folder if it doesn't exists (cél mappa létrehozása, ha nem létezik)
                     int l = 0;
                     while (l < destination.Length && destination.IndexOf("\\", l) >= 0)
                         l = destination.IndexOf("\\", l) + 1;
                     if (!Directory.Exists(destination.Substring(0, destination.Length - destination.Substring(l - 1).Length)))
                         Directory.CreateDirectory(destination.Substring(0, destination.Length - destination.Substring(l - 1).Length));
 
+                    // creating reader and writer object (olvasó és író objektum létrehozása)
                     StreamReader sr = new StreamReader(source, Encoding.GetEncoding("ISO-8859-2"));
                     StreamWriter sw = new StreamWriter(new FileStream(destination, FileMode.OpenOrCreate, FileAccess.ReadWrite), Encoding.UTF8);
+
+                    // iterating trough source file (végigiterálás a forrásfile-on)
                     while (!sr.EndOfStream)
                     {
+                        // current variable stores the current line in the source file (current változó tárolja a jelenlegi sort a forrásfile-ban)
                         string current = sr.ReadLine();
 
+                        // removing comments (kommentek eltávolítása)
                         while (current.Contains("<!--"))
                         {
                             if (current.Contains("-->"))
                             {
+                                // removing single-line comments (1 soros commentek eltávolítása)
                                 current = current.Remove(current.IndexOf("<!--"), (current.IndexOf("-->") + 3) - current.IndexOf("<!--"));
                             }
                             else
                             {
+                                // removing multiple-line comments (többsoros kommentek eltávolítása)
                                 while (!current.Contains("-->") && !sr.EndOfStream)
                                 {
                                     current = sr.ReadLine();
@@ -483,42 +504,59 @@ namespace HTML_compressor
                                 current = current.Remove(0, current.IndexOf("-->") + 3);
                             }
                         }
+                        // removing whitespaces and tabulators (üres helyek és tabulátorok eltávolítása)
                         current = current.TrimStart(' ');
                         current = current.TrimStart('\t');
+
+                        // only write the line if it isn't empty after removing comments and whitespaces (csak akkor íródik ki a sor, ha nem üres az üres helyek, és kommentek eltávolítása után)
                         if (current != "")
                         {
+                            // differentiate the 4 special character used in the keys (a kulcsokban használt 4 speciális karakter megkülönböztetése)
                             current = current.Replace(@"\", @"\\");
                             current = current.Replace(@"&", @"&&");
                             current = current.Replace(@"$", @"$$");
                             current = current.Replace(@"%", @"%%");
 
+                            // compress tags and properties (tagek és tulajdonságok tömörítése)
                             string upperCurrent = current.ToUpper();
                             for (int i = compressData.Count - 1; i >= 0; i--)
                             {
                                 while (upperCurrent.Contains(compressData[i][0]))
                                 {
+                                    // finding the current tag/property in the uppercased current line (a jelenlegi tag/tulajdonság megkeresése a nagybetűsített sorban)
+                                    // because the tags and properties are saved in uppercase characters. (mert a tagek és tulajdonságok nagybetűsen vannak elmentve)
+                                    // Than change the tag/property in the original curret line (utána az eredeti sorban a tag/tulajdonság megváltoztatása)
+                                    // at the same position as in the uppercased line (ugyanabban a pozícióban mint a nagybetűsben)
                                     current = current.Replace(current.Substring(upperCurrent.IndexOf(compressData[i][0]), compressData[i][0].Length), compressData[i][1]);
+
+                                    // syncing the uppercased line variable with the changed line (a nagybetűs sor változó szinkronizálása a megváltozott eredeti sorral)
                                     upperCurrent = current.ToUpper();
                                 }
                             }
+                            // writing the compressed line to the file (tömörített sor fileba írása)
                             sw.WriteLine(current);
                         }
                     }
+                    // closing the reader and writer objects (olvasó és író objektum lezárása)
                     sw.Close();
                     sr.Close();
 
+                    // information gathering about file sizes (információgyűjtés a file-méretekről)
                     FileInfo fileBefore = new FileInfo(source);
                     FileInfo fileAfter = new FileInfo(destination);
+
+                    // succesful compressing -> write out the filesizes (sikeres tömörítés -> fileméretek kiírása)
                     MessageBox.Show(string.Format("Kész!\n\nEredeti méret: {0} B\nTömörített méret: {1} B", fileBefore.Length, fileAfter.Length));
-                    
                 }
                 else
                 {
+                    // error if the source file doesn't exists (hiba, ha a forrás file nem létezik)
                     MessageBox.Show("A megadott file nem létezik: " + source, "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
             }
             catch (Exception ex)
             {
+                // general error catching, preventing the program to stop (általános hiba elkapás, a program leállásának elkerülése érdekében)
                 MessageBox.Show("Hiba: " + ex.Message, "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
@@ -527,46 +565,60 @@ namespace HTML_compressor
         {
             try
             {
+                // set source and destination (forrás és cél beolvasása)
                 source = txtForras.Text;
                 destination = txtCel.Text;
 
                 if (source != null && destination != null && File.Exists(source))
                 {
+                    // create destination folder if it doesn't exists (cél mappa létrehozása, ha nem létezik)
                     int l = 0;
                     while (l < destination.Length && destination.IndexOf("\\", l) >= 0)
                         l = destination.IndexOf("\\", l) + 1;
                     if (!Directory.Exists(destination.Substring(0, destination.Length - destination.Substring(l - 1).Length)))
                         Directory.CreateDirectory(destination.Substring(0, destination.Length - destination.Substring(l - 1).Length));
 
+                    // creating reader and writer object (olvasó és író objektum létrehozása)
                     StreamReader sr = new StreamReader(source, Encoding.GetEncoding("ISO-8859-2"));
                     StreamWriter sw = new StreamWriter(new FileStream(destination, FileMode.OpenOrCreate, FileAccess.ReadWrite), Encoding.UTF8);
+
+                    // iterating trough source file (végigiterálás a forrásfile-on)
                     while (!sr.EndOfStream)
                     {
+                        // current variable stores the current line in the source file (current változó tárolja a jelenlegi sort a forrásfile-ban)
                         string current = sr.ReadLine();
 
+                        // differentiate the 4 special character used in the keys (a kulcsokban használt 4 speciális karakter megkülönböztetése)
                         current = current.Replace(@"\\", @"\");
                         current = current.Replace(@"&&", @"&");
                         current = current.Replace(@"$$", @"$");
                         current = current.Replace(@"%%", @"%");
 
+                        // decompress tags and properties (tagek és tulajdonságok visszafejtése)
                         for (int i = compressData.Count - 1; i >= 0; i--)
                         {
+                            // simply change the keys to the original tag/property (egyszerűen a kulcsok kicserélése a tagre/tulajdonságra)
                             current = current.Replace(compressData[i][1], compressData[i][0]);
                         }
+                        // writing out the decompressed line to the file (visszafejtett sor kiírása fileba)
                         sw.WriteLine(current);
                     }
+                    // closing the reader and writer objects (olvasó és író objektum lezárása)
                     sw.Close();
                     sr.Close();
 
+                    // succesful decompressing (sikeres visszafejtés)
                     MessageBox.Show("Kész!");
                 }
                 else
                 {
+                    // error if the source file doesn't exists (hiba, ha a forrás file nem létezik)
                     MessageBox.Show("A megadott file nem létezik: " + source, "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
             }
             catch (Exception ex)
             {
+                // general error catching, preventing the program to stop (általános hiba elkapás, a program leállásának elkerülése érdekében)
                 MessageBox.Show("Hiba: " + ex.Message, "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
